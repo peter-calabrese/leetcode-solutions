19060a3100094c7403a0d8602e47c1e1020ab54c
Peter Calabrese <peter@caldev.io>
2024-02-15 21:31:02 -0500

    used to run solutions

---
+++ index.js
@@ -0,0 +1,7 @@
+import containsDuplicate from './ContainsDuplicate.js';
+import twoSum from './TwoSum.js';
+import isAnagram from './ValidAnagram.js';
+
+// console.log(containsDuplicate([1, 2, 3, 4]));
+// console.log(twoSum([3, 2, 4], 6));
+// console.log(isAnagram('Anagram', 'gramana'));
