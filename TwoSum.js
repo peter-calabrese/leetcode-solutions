a29800eef99d291b25eee68184caf577d025b378
Peter Calabrese <peter@caldev.io>
2024-02-21 16:18:45 -0500

    TwoSum var to let

--- TwoSum.js
+++ TwoSum.js
@@ -1,6 +1,6 @@
 const twoSum = (nums, target) => {
   const map = new Map();
-  for (var i = 0; i < nums.length; i++) {
+  for (let i = 0; i < nums.length; i++) {
     const val = target - nums[i];
     if (map.has(val)) {
       return [map.get(val), i];
