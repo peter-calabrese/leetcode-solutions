12e02850c649219708e24eae485033968d0fac0f
Peter Calabrese <peter@caldev.io>
2024-02-21 16:18:31 -0500

    getContatenation run

--- index.js
+++ index.js
@@ -1,7 +1,10 @@
 import containsDuplicate from './ContainsDuplicate.js';
 import twoSum from './TwoSum.js';
 import isAnagram from './ValidAnagram.js';
+import getConcatenation from "./getConcatenation.js";
 
 // console.log(containsDuplicate([1, 2, 3, 4]));
 // console.log(twoSum([3, 2, 4], 6));
 // console.log(isAnagram('Anagram', 'gramana'));
+// console.log(getConcatenation([1,2,1]))
+
